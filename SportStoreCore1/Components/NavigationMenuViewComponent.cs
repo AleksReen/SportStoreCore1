ebffea9eb54bd617e2d8f4fc04306d8801f6a7da
AleksReen <aleksandr.reentovich@tut.by>
2018-12-30 15:53:17 +0300

    NavMenu

---
+++ SportStoreCore1/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStoreCore1.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStoreCore1.Components
+{
+    public class NavigationMenuViewComponent: ViewComponent
+    {
+        private IProductRepository productRepository;
+
+        public NavigationMenuViewComponent(IProductRepository repository)
+        {
+            productRepository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(productRepository.Products
+                .Select(prod => prod.Category)
+                .Distinct()
+                .OrderBy(prod => prod));
+        }     
+    }
+}
