71edeba08e5673cb3a87cc83c844d0086c88ccbc
AleksReen <aleksandr.reentovich@tut.by>
2019-01-02 14:52:30 +0300

    Add Cart

---
+++ SportStoreCore1/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportStoreCore1.Infrastructure;
+using SportStoreCore1.Models;
+using SportStoreCore1.Models.Interfaces;
+using SportStoreCore1.Models.ViewModels;
+
+namespace SportStoreCore1.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View ( new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            var product = repository.Products.Where(p => p.ProductID == productId).FirstOrDefault();
+
+            if (product != null)
+            {
+                var cart = GetCart();
+                cart.AddOrUpdateCartLine(product, 1);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            var product = repository.Products.Where(p => p.ProductID == productId).FirstOrDefault();
+
+            if (product != null)
+            {
+                var cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+
+        private Cart GetCart()
+        {
+            return HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+        }
+    }
+}
---
+++ SportStoreCore1/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStoreCore1.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest httpRequest)
+        {
+            var result = httpRequest.QueryString.HasValue ? $"{httpRequest.Path} {httpRequest.QueryString}" : httpRequest.Path.ToString();
+            return result;
+        }       
+    }
+}
